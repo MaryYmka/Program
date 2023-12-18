77e10e7b761f7444b61408fa8668acf580e81ba3
Maria <skorynina.m@yandex.ru>
2023-12-18 23:45:52 +0300

    Гири

---
+++ Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 1;
+int numberB = 2;
+int numberC = 6;
+int numberD = 8;
+int numberE = 4;
+
+int max = numberA;
+
+if(numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+if (numberD > max) max = numberD;
+if (numberE > max) max = numberE;
+
+Console.Write("max = ");
+Console.WriteLine(max);
